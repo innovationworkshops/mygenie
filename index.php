1d6204939243b93fa57d4dafd4dce7435092eff5
lightningexperience <lightningexperience@users.noreply.github.com>
2023-02-09 14:22:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <body>
     <!--Navigation -->
     <div style="margin: 30px; margin-top: 50px;"> <center> 
-      <h1 >  TD Bank Portal </h1> 
+      <h1 > Bank Portal </h1> 
        <h3> <i>powered by</i> Genie Data Cloud  <h3>  
       <img src="wide-characters.jpg"> 
       </div>  </center>
