db15492bb22fd8b451ddbba9b63da51028d3525e
lightningexperience <lightningexperience@users.noreply.github.com>
2023-02-10 00:49:31 -0500

    Update db.php

--- db.php
+++ db.php
@@ -32,7 +32,7 @@ $dbname = ltrim($dbparts['path'],'/');
                             first_name VARCHAR (20),
   deviceId varchar(80),
   eventId varchar(80),
-  dateTimefield DEFAULT CURRENT_TIMESTAMP,
+  dateTimefield DATETIME NOT NULL DEFAULT NOW(),
   eventType varchar(80),
   category varchar(80),
   sessionId varchar(80)
